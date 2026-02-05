7f97f3155916779327b470b371a2bb1f0519e36a
囫砸 <3361107168@qq.com>
2026-02-05 15:58:40 +0800

    Update passwall2 repository URL in feed source
    修复链接失效的问题

--- 2410-diy-part1-pw.sh
+++ 2410-diy-part1-pw.sh
@@ -16,7 +16,7 @@
 # Add a feed source
 # echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 sed -i '1i src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main' feeds.conf.default
-sed -i '2i src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main' feeds.conf.default
+sed -i '2i src-git passwall2 https://github.com/Openwrt-Passwall/openwrt-passwall2.git;main' feeds.conf.default
 echo >> feeds.conf.default
 echo 'src-git istore https://github.com/linkease/istore;main' >> feeds.conf.default
     # feeds获取源码：
